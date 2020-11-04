c90d8fe9a1f82d16782ba626f17cbc82abd4f49c
padma <yaswin302015@gmail.com>
2020-11-04 16:27:00 -0500

    new file

---
+++ src/addition/addition1.java
@@ -0,0 +1,22 @@
+package addition;
+
+public class addition1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=900, b=100;
+		int c=a+b;
+    int d=a-b;
+    float g=78.285985f+52.255545f;
+    boolean h=true, i=false;
+    boolean j=h&&i;
+    boolean k=h||i;
+    boolean l=!h;
+    int z=a*b;
+    System.out.println("c="+c+"\n"+"z="+z+"\n"+"d="+d+"\n"+"g="+g+"\n"+"j="+j+"\n"+"k="+k+"\n"+"l="+l);
+	;
+ char e='p';
+ System.out.println("e="+e);
+	}
+
+}
